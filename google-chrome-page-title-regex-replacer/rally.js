4243aa25209363d4219ab955e29e9e1fc982dc53
CJ <lim@chernjie.com>
2013-12-12 16:00:39 +0800

    US1: Successfully created chrome extension;

---
+++ google-chrome-page-title-regex-replacer/rally.js
@@ -0,0 +1,9 @@
+var rallyUrl = 'https://rally1.rallydev.com/slm/rally.sp?#/search?keywords=';
+var els = document.querySelectorAll('.commit-title, .message');
+for (var i in els)
+{
+	if (els[i] && els[i].innerHTML) 
+		els[i].innerHTML = els[i].innerHTML
+			.replace(/(US|DE|TA)([0-9]+)/g
+				, '<a href="' + rallyUrl + '$1$2">$1$2</a>');
+}
