5882dab86e2bc5c91df9b4c9cbfba197e5473f12
CJ <lim@chernjie.com>
2013-12-12 17:55:45 +0800

    Add colors;

--- google-chrome-page-title-regex-replacer/rally.js
+++ google-chrome-page-title-regex-replacer/rally.js
@@ -5,5 +5,5 @@ for (var i in els)
 	if (els[i] && els[i].innerHTML) 
 		els[i].innerHTML = els[i].innerHTML
 			.replace(/(US|DE|TA)([0-9]+)/g
-				, '<a href="' + rallyUrl + '$1$2">$1$2</a>');
+				, '<a style="text-decoration:underline; color:purple;" href="' + rallyUrl + '$1$2">$1$2</a>');
 }
