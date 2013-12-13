006e8cbba44fd91cae5acaafe20dacda360e11d4
CJ <lim@chernjie.com>
2013-12-13 12:14:58 +0800

    US1: Rebuild rally;

--- google-chrome-page-title-regex-replacer/rally.js
+++ google-chrome-page-title-regex-replacer/rally.js
@@ -1,9 +1,11 @@
 var rallyUrl = 'https://rally1.rallydev.com/slm/rally.sp?#/search?keywords=';
-var els = document.querySelectorAll('.commit-title .message, .message .message');
+var els = document.querySelectorAll('a.message, .commit-title a, .message, .commit-title');
 for (var i in els)
 {
-	if (els[i] && els[i].innerHTML) 
-		els[i].innerHTML = els[i].innerHTML
-			.replace(/(US|DE|TA)([0-9]+)/g
-				, '<a style="text-decoration:underline; color:purple;" href="' + rallyUrl + '$1$2">$1$2</a>');
+	if (els[i] && els[i].innerHTML && ! els[i].innerHTML.match(/rally-link/g))
+	{
+		replacement = '<a class="issue-link rally-link" href="' + rallyUrl + '$1$2">$1$2</a>';
+		els[i].outerHTML = els[i].outerHTML
+			.replace(/(US|DE|TA)([0-9]+)/g, replacement);
+	}
 }
